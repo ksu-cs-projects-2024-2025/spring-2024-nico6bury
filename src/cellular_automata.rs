06f6a4dafc7cfdb4766d4c74da63033487434b5f
Nicholas Sixbury <nico6bury@ksu.edu>
2024-03-23 22:24:05 -0500

    Added a few helpful functions and documentation to Square and SquareGrid.

--- src/cellular_automata.rs
+++ src/cellular_automata.rs
@@ -1,5 +1,8 @@
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone, Ord, PartialOrd, Hash)]
+/// Represents an atomic rectangle of pixels for use in cellular automata generations.
+/// In this case, [x] and [y] refer to the x and y of ther pixel in the upper left corner 
+/// of the [Square] in regards to the full image this [Square] is a part of.
 pub struct Square {
 	x: usize,
 	y: usize,
@@ -8,6 +11,7 @@ pub struct Square {
 	color: (u8,u8,u8),
 }//end struct Square
 
+#[allow(dead_code)]
 impl Square {
 	pub fn new(x: usize, y: usize, width: usize, height: usize) -> Square {
 		Square {
@@ -19,8 +23,8 @@ impl Square {
 		}//end struct init
 	}//end new()
 
-	// /// Allows easy setting of color with new Square.
-	// pub fn with_color(mut self, color: (u8,u8,u8)) -> Square { self.color = color; return self; }
+	/// Allows easy setting of color with new Square.
+	pub fn with_color(mut self, color: (u8,u8,u8)) -> Square { self.color = color; return self; }
 
 	/// Sets the color of this square
 	pub fn set_color(&mut self, color: (u8,u8,u8)) { self.color = color; }
@@ -33,12 +37,18 @@ impl Square {
 }//end impl for Square
 
 #[derive(Debug, PartialEq, Eq, Clone, Ord, PartialOrd, Hash)]
+/// Represents all the [Squares] that represent the groups within a full image.  
+/// Once loaded into a SquareGrid, the record of image dimensions can't be changed, nor can the 
+/// positioning of squares.  
+/// This is so that a record is always kept of the dimensions of the image when the Square data 
+/// was originally taken.
 pub struct SquareGrid {
 	squares: Vec<Square>,
 	img_width: usize,
 	img_height: usize,
 }//end struct SquareGrid
 
+#[allow(dead_code)]
 impl SquareGrid {
 	pub fn from_squares(squares: Vec<Square>, img_width: usize, img_height: usize) -> SquareGrid {
 		// TODO: Parse squares to ensure everything fits within img_bounds
@@ -51,7 +61,15 @@ impl SquareGrid {
 
 	/// Allows immutable iteration through squares
 	pub fn iter(&self) -> std::slice::Iter<'_, Square> { self.squares.iter() }
+	/// Allows mutable iteration through squares
 	pub fn iter_mut(&mut self) -> std::slice::IterMut<'_, Square> { self.squares.iter_mut() }
+	/// Width of original image when square info was taken.
 	pub fn img_width(&self) -> &usize { &self.img_width }
+	/// Height of original image when square info was taken.
 	pub fn img_height(&self) -> &usize { &self.img_height }
+
+	/// Gets a reference to the square at the specified location
+	pub fn get(&self, row: &usize, col: &usize) -> &Square { &self.squares[col * self.img_width + row] }
+	/// Gets a mutable reference to the square at the specified location
+	pub fn get_mut(&mut self, row: &usize, col: &usize) -> &mut Square { &mut self.squares[col * self.img_width + row] }
 }//end impl for SquareGrid
